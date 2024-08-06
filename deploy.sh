0e5078c8d5e1f1d4509a824bb055ee59e4f9eba5
Dean Hiller <dean.hiller@tray.com>
2024-08-06 08:57:53 +0300

    deploy script working

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 export TAG=latest
 
+gcloud auth login
+gcloud config set project biltup-community
 gcloud auth configure-docker us-central1-docker.pkg.dev
-docker buildx build .
+docker buildx build -t us-central1-docker.pkg.dev/biltup-community/biltup-repo/prototype-image:${TAG} .
 
-docker tag 95eee2402110 us-central1-docker.pkg.dev/deansprototype/ts-prototype/prototype-image:${TAG}
+## We just tag above during the build of the docker image instead
+## We could tag with version here in addition to latest.  We MUST use latest as our deploy chooses latest to deploy
+####docker tag ${hashOfBuiltImage} us-central1-docker.pkg.dev/biltup-community/biltup-repo/prototype-image:${TAG}
 
-docker push us-central1-docker.pkg.dev/deansprototype/ts-prototype/prototype-image:${TAG}
+docker push us-central1-docker.pkg.dev/biltup-community/biltup-repo/prototype-image:${TAG}
